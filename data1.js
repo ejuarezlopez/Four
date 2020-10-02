f05501886fdb3e1700a3b707e027ff39111b78f6
ejuarezlopez <70125208+ejuarezlopez@users.noreply.github.com>
2020-10-01 20:50:29 -0500

    Add files via upload

---
+++ data1.js
@@ -0,0 +1,29 @@
+var data1 = {
+"type": "FeatureCollection",
+"name": "volcangeojson",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Acatenango", "ELEV": 3976.0, "TYPE": "Stratovolcano" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -90.876004958162298, 14.501002009235322 ] ] } },
+{ "type": "Feature", "properties": { "NAME": "Fuego", "ELEV": 3763.0, "TYPE": "Stratovolcano" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -90.880002762117783, 14.473000216187017 ] ] } },
+{ "type": "Feature", "properties": { "NAME": "Chiquimula Volc Field", "ELEV": 1192.0, "TYPE": "Cinder cones" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -89.550008190025949, 14.83000065340741 ] ] } },
+{ "type": "Feature", "properties": { "NAME": "Quezaltepeque", "ELEV": 1200.0, "TYPE": "Volcanic field" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -89.450002061346026, 14.570000352834029 ] ] } },
+{ "type": "Feature", "properties": { "NAME": "Ipala", "ELEV": 1650.0, "TYPE": "Stratovolcano" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -89.630002416578236, 14.550000845162856 ] ] } },
+{ "type": "Feature", "properties": { "NAME": "Agua", "ELEV": 3760.0, "TYPE": "Stratovolcano" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -90.743001686478635, 14.465002699914635 ] ] } },
+{ "type": "Feature", "properties": { "NAME": "Pacaya", "ELEV": 2552.0, "TYPE": "Complex volcano" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -90.601003357918614, 14.381001134794531 ] ] } },
+{ "type": "Feature", "properties": { "NAME": "Ixtepeque", "ELEV": 1292.0, "TYPE": "Lava domes" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -89.680005483166497, 14.420001649056871 ] ] } },
+{ "type": "Feature", "properties": { "NAME": "Suchitan", "ELEV": 2042.0, "TYPE": "Stratovolcanoes" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -89.780003984696123, 14.400002140486379 ] ] } },
+{ "type": "Feature", "properties": { "NAME": "Santiago, Cerro", "ELEV": 1192.0, "TYPE": "Volcanic field" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -89.870007977685987, 14.330001473689038 ] ] } },
+{ "type": "Feature", "properties": { "NAME": "Chingo", "ELEV": 1775.0, "TYPE": "Stratovolcano" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -89.730008551553397, 14.120001377161827 ] ] } },
+{ "type": "Feature", "properties": { "NAME": "Tahual", "ELEV": 1716.0, "TYPE": "Stratovolcano" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -89.90000676480031, 14.430000929399341 ] ] } },
+{ "type": "Feature", "properties": { "NAME": "Flores", "ELEV": 1600.0, "TYPE": "Volcanic field" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -90.000005267229255, 14.300002687474034 ] ] } },
+{ "type": "Feature", "properties": { "NAME": "Moyuta", "ELEV": 1662.0, "TYPE": "Stratovolcano" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -90.100003771456898, 14.030001203592633 ] ] } },
+{ "type": "Feature", "properties": { "NAME": "Almolonga", "ELEV": 3197.0, "TYPE": "Stratovolcano" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -91.480009026495338, 14.820002343433373 ] ] } },
+{ "type": "Feature", "properties": { "NAME": "Santa Maria", "ELEV": 3772.0, "TYPE": "Stratovolcano" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -91.552007642438753, 14.756000290480983 ] ] } },
+{ "type": "Feature", "properties": { "NAME": "Santo Tom s", "ELEV": 3542.0, "TYPE": "Stratovolcanoes" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -91.479001946782375, 14.710000750234428 ] ] } },
+{ "type": "Feature", "properties": { "NAME": "Tajumulco", "ELEV": 4220.0, "TYPE": "Stratovolcano" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -91.903005660782583, 15.034003105558838 ] ] } },
+{ "type": "Feature", "properties": { "NAME": "Cuilapa-Barbarena", "ELEV": 1454.0, "TYPE": "Volcanic field" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -90.400006904095335, 14.330001478185636 ] ] } },
+{ "type": "Feature", "properties": { "NAME": "Tecuamburro", "ELEV": 1845.0, "TYPE": "Stratovolcano" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -90.407003062141598, 14.15600164515979 ] ] } },
+{ "type": "Feature", "properties": { "NAME": "Toliman", "ELEV": 3158.0, "TYPE": "Stratovolcano" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -91.189008582380154, 14.612001149333537 ] ] } },
+{ "type": "Feature", "properties": { "NAME": "Atitlan", "ELEV": 3535.0, "TYPE": "Stratovolcano" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -91.186002600239817, 14.583001812983241 ] ] } }
+]
+}
